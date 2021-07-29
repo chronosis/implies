fea19c70a7b5c1e8f6b1e59b999844ec86038c02
chronosis <jayreardon@gmail.com>
2021-07-29 00:15:44 -0600

    fix: correcting type defs

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module 'implies';
 
 export function implies(left: any, right: any): boolean;
-export function bitwiseImplies(left: any, right: any, radix: number = 10): string;
+export function bitwiseImplies(left: any, right: any, radix?: number): string;
